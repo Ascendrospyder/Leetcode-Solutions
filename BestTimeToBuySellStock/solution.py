6de1e630302e107022a9ce25269e691cf327bb52
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2024-11-28 18:54:20 +1100

    Add two-pointer solution for Best Time to Buy and Sell Stock problem

--- BestTimeToBuySellStock/solution.py
+++ BestTimeToBuySellStock/solution.py
@@ -9,6 +9,20 @@ class Solution(object):
     
     return max_profit
   
+  def maxProfitTwoPointer(self, prices):
+    l, r = 0, 1
+    max_profit = 0
+
+    while r < len(prices):
+      if prices[l] < prices[r]:
+        max_profit = max(max_profit, prices[r] - prices[l])
+      else:
+        l = r
+      
+      r += 1
+    
+    return max_profit
+  
 # Example usage
 prices = [7, 1, 5, 3, 6, 4]
 solution = Solution()
